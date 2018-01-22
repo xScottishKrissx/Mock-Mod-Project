f68be93e92b3f6ffde46691597ff7dcabdff091b
xScottishKrissx <chrisdunne66@gmail.com>
2018-01-22 19:20:29 +0000

    Day 1 - Start of Basic Outline

---
+++ header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>NewProject1</title>
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="css/index.css" >
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+
+<?php include ("nav.php") ?>
--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-<div class="myDiv"></div>
+<?php include ("header.php"); ?>
+
+
+  <!--
+  <div class="myDiv">
+    <h1 class="red">Hello World</h1>
+    <p class="green">My Name is Christopher</p>
+    <span class="blue">Welcome to my New Project</span>
+  </div>
+-->
+
+
+<div class="container">
+
+<div class="row introBox">
+  <h1>Intro Box</h1>
+</div>
+
+<div class="row storeBox">
+  <h1>Store Box</h1>
+</div>
+
+<div class="row aboutBox">
+  <h1>About Box</h1>
+</div>
+
+<div class="row footer">
+  <div class="">
+    <h1>Footer</h1>
+  </div>
+</div>
+
+
+
+
+
+
+</div>
+
+
+<?php include ("footer.php"); ?>
