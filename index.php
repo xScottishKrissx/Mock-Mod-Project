00a84b261f6d803c83dd271e78a552fa8aa71dfe
xScottishKrissx <chrisdunne66@gmail.com>
2018-01-23 18:37:56 +0000

    Day 2 - Finished Basic Strcture and Decided on Content

--- index.php
+++ index.php
@@ -12,16 +12,28 @@
 
 <div class="container">
 
-<div class="row introBox">
-  <h1>Intro Box</h1>
+<div id="introBoxAnchor" class="row introBox">
+  <h1>Dreich</h1>
+  <div>
+    <span>"adjective - Scottish" </span>
+    <span>"a cold,dreich early april day"</span>
+  </div>
 </div>
 
-<div class="row storeBox">
-  <h1>Store Box</h1>
+<div id="storeBoxAnchor" class="row storeBox">
+  <h1>Weather Mods</h1>
+  <div>
+    Bored of the sun? <br/><br/> Have a hatred for all things "humane"?<br/><br/> Then this is the god-mod-pack for you. All sorts of weather modifications that you can stare out the window and complain at. Comes with special add ons including "that smell when rain hits the concrete" and "people discussing how it's going to liven up later on". Don't fortget the optional extra: horizontal rain!
+    <div class="link">Download Mods...</div>
+  </div>
 </div>
 
-<div class="row aboutBox">
-  <h1>About Box</h1>
+<div id="aboutBoxAnchor" class="row aboutBox">
+  <h1>The Team</h1>
+  <div>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue enim lectus, pulvinar imperdiet augue auctor vitae. Etiam et leo eu nisl placerat gravida. Praesent quam odio, feugiat ut nisi non, ullamcorper luctus arcu.
+    <div class="link">Contact Us...</div>
+ </div>
 </div>
 
 <div class="row footer">
