113e8c8cb5fd19e2f772c0fe0f711121c06fceaf
xScottishKrissx <chrisdunne66@gmail.com>
2018-02-01 18:07:33 +0000

    Day 9 - Home Page Styling

--- index.php
+++ index.php
@@ -24,16 +24,34 @@
   <h1>Weather Mods</h1>
   <div>
     Bored of the sun? <br/><br/> Have a hatred for all things "humane"?<br/><br/> Then this is the god-mod-pack for you. All sorts of weather modifications that you can stare out the window and complain at. Comes with special add ons including "that smell when rain hits the concrete" and "people discussing how it's going to liven up later on". Don't fortget the optional extra: horizontal rain!
-    <div class="link">Download Mods...</div>
+    <a href="store.php"><div class="link">View  Mods...</div></a>
   </div>
 </div>
 
 <div id="aboutBoxAnchor" class="row aboutBox">
+
   <h1>The Team</h1>
-  <div>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue enim lectus, pulvinar imperdiet augue auctor vitae. Etiam et leo eu nisl placerat gravida. Praesent quam odio, feugiat ut nisi non, ullamcorper luctus arcu.
-    <div class="link">Contact Us...</div>
- </div>
+   <div class="aboutText">
+
+     <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue enim lectus, pulvinar imperdiet augue auctor vitae. Etiam et leo eu nisl placerat gravida. Praesent quam odio, feugiat ut nisi non, ullamcorper luctus arcu.
+    </p>
+      <div class="circles">
+        <img src="img/placeholder.jpg" alt="" />
+        <img src="img/placeholder.jpg" alt="" />
+      </div>
+      <div class="circles">
+        <img src="img/placeholder.jpg" alt="" />
+        <img src="img/placeholder.jpg" alt="" />
+      </div>
+      <div class="circles">
+        <img src="img/placeholder.jpg" alt="" />
+        <img src="img/placeholder.jpg" alt="" />
+      </div>
+      <a href="about.php"><div class="link">Contact Us...</div></a>
+
+   </div>
+
 </div>
 
 
